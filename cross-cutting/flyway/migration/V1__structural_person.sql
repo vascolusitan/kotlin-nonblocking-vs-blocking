4bfe03de997573a1bcdf717fb5c9b524dcfbadca
Vasco Lusitano <vasco.lusitano@five9.com>
2024-10-19 11:41:42 +0100

    flyway changes

--- cross-cutting/flyway/migration/V1__structural_person.sql
+++ cross-cutting/flyway/migration/V1__structural_person.sql
@@ -3,9 +3,3 @@ CREATE TABLE IF NOT EXISTS person (
     name VARCHAR(255) NOT NULL,
     age INT NOT NULL
 );
-
-INSERT INTO person (name, age) VALUES
-   ('Vasco', 27),
-   ('Rita', 26),
-   ('Jojo', 35),
-   ('Mafalda', 28);
---
+++ cross-cutting/flyway/migration/V2__data_person.sql
@@ -0,0 +1,5 @@
+INSERT INTO person (name, age) VALUES
+   ('Vasco', 27),
+   ('Rita', 26),
+   ('Jojo', 35),
+   ('Mafalda', 28);
