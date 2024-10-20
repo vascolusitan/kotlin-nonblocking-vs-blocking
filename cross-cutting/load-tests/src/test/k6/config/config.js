1d6ca730421e6b865a1a3a902241c67785a1d848
Vasco Lusitano <vasco.lusitano@five9.com>
2024-10-20 16:17:39 +0100

    load tests changes

--- cross-cutting/load-tests/src/test/k6/config/config.js
+++ cross-cutting/load-tests/src/test/k6/config/config.js
@@ -1,7 +1,18 @@
 export let options = {
-    vus: 50,                                // Number of virtual users
-    duration: '30s',                        // Duration of the test
-    /*thresholds: {
-        http_req_duration: ['p(95)<200'],   // Example threshold: 95% of requests should be under 200ms
-    },*/
+    stages: [
+        { duration: '2m', target: 25 },   // Ramp up to 25 concurrent connections
+        { duration: '10m', target: 25 },  // Hold for 10 minutes at 25 connections
+        { duration: '2m', target: 0 },    // Ramp down to 0
+        { duration: '2m', target: 100 },  // Ramp up to 100 concurrent connections
+        { duration: '10m', target: 100 }, // Hold for 10 minutes at 100 connections
+        { duration: '2m', target: 0 },    // Ramp down to 0
+        { duration: '2m', target: 300 },  // Ramp up to 300 concurrent connections
+        { duration: '10m', target: 300 }, // Hold for 10 minutes at 300 connections
+        { duration: '2m', target: 0 },    // Ramp down to 0
+    ],
+    thresholds: {
+        http_reqs: [`count>=5000000`], // Ensure the test runs for at least 5 million requests
+        http_req_duration: ['p(95)<200'], // 95% of requests should complete in less than 200ms
+        'http_req_failed{expected_response:true}': ['rate<0.01'], // Less than 1% failures
+    }
 };
